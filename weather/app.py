268aff749b7a1edbd81e69d628ccb6a6ec7fdf60
石头城 <778042395@qq.com>
2018-12-19 18:36:14 +0800

    测试

---
+++ weather/app.py
@@ -0,0 +1,12 @@
+import pymongo
+from flask import Flask, url_for, render_template, redirect, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
